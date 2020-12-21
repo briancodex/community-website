8daddbbaafb847a5d8fcb5c58ef1767e42bcaa1b
aryan-jha-18 <jhaaa1010@gmail.com>
2020-12-21 09:54:29 +0530

    Created Animated Full Screen Navbar
    Added full screen animated responsive navigation using html css and gsap.
    Also added a custom cursor for hover on navigation link with cool and modern hover effect.
    Added PlayFair Display font.
    Gsap library added.
    Shifted script tag to top with "defer" attribute which would beahve the same no issues with that

---
+++ app.js
@@ -0,0 +1,58 @@
+
+const navBtn = document.querySelector('.menu-toggle');
+const cursor = document.querySelector('.cursor');
+const navLinks = document.querySelectorAll('.nav-link');
+
+// ***********CURSOR HOVER ANIMATION***************
+
+window.addEventListener('mousemove',cursorMove);
+
+function cursorMove(e){
+ cursor.style.top = `${e.pageY}px`
+ cursor.style.left = `${e.pageX}px`
+}
+
+// **********FULL SCREEN NAVIGATION ANIMATION**************
+
+const tl = gsap.timeline({defaults:{duration: 1},paused:true,reversed:true});
+
+tl
+.from('.navigation-menu',{top: '-100%'})
+.to('.nav-overlay',{width: '100%',stagger: 0.1})
+.from('.nav-item',{y:'100px',rotate: '10',opacity: '0',stagger : 0.2});
+
+navBtn.addEventListener('click',function(){
+    
+    if(navBtn.classList.contains('active')){
+        tl.timeScale(1.5);
+        tl.reverse()
+    }
+    else{
+        tl.timeScale(1);
+        tl.play();
+    }
+     navBtn.classList.toggle('active');
+
+});
+
+navLinks.forEach(function(navLink){
+    
+    navLink.addEventListener('click',function(){
+        
+        tl.timeScale(1.5);
+        tl.reverse();
+        navBtn.classList.remove('active');
+    })
+
+    navLink.addEventListener('mouseleave',function(){
+        cursor.classList.remove('cursor-active');
+        d
+        document.body.style.cursor = 'initial';
+        
+    })
+
+    navLink.addEventListener('mouseover',function(){
+        cursor.classList.add('cursor-active');
+        ocument.body.style.cursor = 'none';
+    })
+})
